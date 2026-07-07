e5809c2419af59439c37d3ef42365ec1181ca39f
Gabriel Room <gabrielbuffonisilva@gmail.com>
2026-07-07 14:56:17 -0300

    Add funcionalidade dos botões nextEp, previousEp e TodosEpisodios da page anime-ep

--- backend/server.js
+++ backend/server.js
@@ -54,7 +54,84 @@ app.get('/episodes/:id', (req, res) => {
 });
 
 
+// Rota para pegar todos episódios de um anime
+app.get('/episodes/anime/:animeId', (req, res) => {
 
+    const animeId = req.params.animeId;
+
+    db.query(
+        "SELECT * FROM episodes WHERE anime_id = ?",
+        [animeId],
+        (err, results) => {
+
+            if (err) {
+                return res.status(500).json(err);
+            }
+
+            res.json(results);
+        }
+    );
+
+});
+
+
+// Rota Next Ep
+
+app.get('/episodes/:id/next', (req, res) => {
+
+    const id = req.params.id;
+
+    db.query(
+        `
+        SELECT e2.*
+        FROM episodes e1
+        JOIN episodes e2
+            ON e1.anime_id = e2.anime_id
+        WHERE e1.id = ?
+        AND e2.episode = e1.episode + 1
+        LIMIT 1
+        `,
+        [id],
+        (err, results) => {
+
+            if (err)
+                return res.status(500).json(err);
+
+            res.json(results[0] || null);
+
+        }
+    );
+
+});
+
+
+// Rota Previous Ep
+app.get('/episodes/:id/previous', (req, res) => {
+
+    const id = req.params.id;
+
+    db.query(
+        `
+        SELECT e2.*
+        FROM episodes e1
+        JOIN episodes e2
+            ON e1.anime_id = e2.anime_id
+        WHERE e1.id = ?
+        AND e2.episode = e1.episode - 1
+        LIMIT 1
+        `,
+        [id],
+        (err, results) => {
+
+            if (err)
+                return res.status(500).json(err);
+
+            res.json(results[0] || null);
+
+        }
+    );
+
+});
 
 
 
--- frontend/script.js
+++ frontend/script.js
@@ -20,12 +20,54 @@ function goToEpisode(id){
     window.location.href = `anime-ep.html?ep-id=${id}`;
 
 }
+// Função goToSinopse
+function goToSinopse(id){
 
-function goToNextEpisode(animeId, episodeId, epAnime){
-
-    window.location.href = `anime-ep.html?anime-id=${animeId}&ep-id=${episodeId}&ep-anime=${epAnime}`;
+    window.location.href = `anime-sinopse.html?anime-id=${id}`;
 
 }
+
+
+// Funcao goToNextEpisode
+function goToNextEpisode(id){
+
+    fetch(`/episodes/${id}/next`)
+        .then(res => res.json())
+        .then(ep => {
+
+            if(!ep){
+                alert("Último episódio!");
+                return;
+            }
+
+            goToEpisode(ep.id);
+
+        });
+
+}
+
+
+// Funcao goToPreviousEpisode
+function goToPreviousEpisode(id){
+
+    fetch(`/episodes/${id}/previous`)
+        .then(res => res.json())
+        .then(ep => {
+
+            if(!ep){
+                alert("Primeiro episódio!");
+                return;
+            }
+
+            goToEpisode(ep.id);
+
+        });
+
+}
+
+
+
+
 
 // Função goToMain
 function goToMain(){
@@ -99,36 +141,11 @@ fetch('/episodes_temporada')
 
 
 
-// Anime-Sinopse 
-
-
-fetch('/episodes')
-    .then(res => res.json())
-    .then(data => {
-        const container = document.querySelector('.episodes-sinopse');
-
-        data.forEach(ep => {
-
-            const dataFormatada = new Date(ep.release_date).toLocaleDateString('pt-BR');
-
-            container.innerHTML += `
-                <div class="episode-card-sinopse">
-                    <img src="/resources/images/${ep.image}">
-                    <p class="episode-card-title-anime">${ep.anime}</p>
-                    <p>Lançamento ${dataFormatada}</p>
-                    <p class="episode-card-p-ep">${ep.episode}</p>
-                </div>
-            `;
-        });
-    });
 
 
 
 
-
-
-
-
+// Anime-Ep
     fetch(`/episodes/${episodeId}`)
     .then(res => res.json())
     .then(episodioAtual => {
@@ -159,11 +176,11 @@ fetch('/episodes')
                 </div>
 
                 <div class="video-button">
-                        <button class="btn-step">Anterior</button>
+                        <button class="btn-step" onclick="goToPreviousEpisode(${episodioAtual.id})">Anterior</button>
 
-                        <button class="btn-list"><i class="bi bi-list">Todos Episódios</i></button>
+                        <button class="btn-list" onclick="goToSinopse(${episodioAtual.anime_id})"><i class="bi bi-list">Todos Episódios</i></button>
                         
-                        <button class="btn-step">Próximo</button>
+                        <button class="btn-step" onclick="goToNextEpisode(${episodioAtual.id})">Próximo</button>
                 </div>
 
 
@@ -188,3 +205,64 @@ fetch('/episodes')
         `;
 
     });
+
+
+
+
+// Anime-Sinopse 
+
+
+fetch(`/episodes/anime/${animeId}`)
+    .then(res => res.json())
+    .then(episodio => {
+        const container = document.querySelector('.anime-sinopse');
+        // Dados do anime (pega o primeiro episódio)
+        console.log(container);
+        const anime = episodio[0];
+
+        // Parte fixa
+        container.innerHTML = `
+            <div class="anime-sinopse-card">
+                <img src="/resources/images/${anime.image_default}">
+
+                <div class="anime-sinopse-info">
+                    <h1>${anime.anime}</h1>
+
+                    <h2>
+                        ${anime.sinopse}
+                    </h2>
+
+                    <div class="anime-sinopse-info-genero">
+                        <span class="genero">Ação</span>
+                        <span class="genero">Fantasia</span>
+                        <span class="genero">Drama</span>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="episodes-list"></div>
+
+
+        `;
+
+        
+        const lista = document.querySelector(".episodes-list");
+
+        episodio.forEach(ep => {
+
+            const dataFormatada = new Date(ep.release_date)
+                .toLocaleDateString("pt-BR");
+
+            lista.innerHTML += `
+                <div class="episode-card-sinopse">
+                    <img src="/resources/images/${ep.image}">
+                    <p class="episode-card-sinopse-title-anime">${ep.anime}</p>
+                    <p>Lançamento ${dataFormatada}</p>
+                    <p>Episódio ${ep.episode}</p>
+                </div>
+            `;
+
+        });
+    });
+
+
